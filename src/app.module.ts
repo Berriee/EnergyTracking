3cd502c80e4814635ca160096c35a59bfe26d1b9
Marcel <marcel.achtert@outlook.com>
2023-09-25 14:34:37 +0200

    works

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BullModule } from '@nestjs/bull';
 import { OnChainCertificateEntities } from '@energyweb/origin-247-certificate';
+import { OffChainCertificateEntities } from '@energyweb/origin-247-certificate';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< Updated upstream
-=======
 import { OnChainCertificateModule } from '@energyweb/origin-247-certificate';
 import { OnChainController } from './on-chain/on-chain.controller';
 import { OnChainService } from './on-chain/on-chain.service';
->>>>>>> Stashed changes
 
 @Module({
   imports: [
+    
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
         host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'test',
-        entities: [...OnChainCertificateEntities],
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'origin',
+        entities: [...OnChainCertificateEntities, ...OffChainCertificateEntities],
         synchronize: true,
     }),
     BullModule.forRoot({
@@ -29,14 +28,9 @@ import { OnChainService } from './on-chain/on-chain.service';
         port: 6379,
       },
     }),
+    OnChainCertificateModule,
   ],
-<<<<<<< Updated upstream
-  controllers: [AppController],
-  providers: [AppService],
-=======
   controllers: [AppController, OnChainController],
   providers: [AppService, OnChainService],
-  
->>>>>>> Stashed changes
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OnChainCertificateFacade } from '@energyweb/origin-247-certificate';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< Updated upstream
-  await app.listen(3000);
-=======
   const blockchainFacade = await app.resolve<OnChainCertificateFacade>(OnChainCertificateFacade);
   await blockchainFacade.deploy();
   app.listen(3000);
->>>>>>> Stashed changes
 }
 bootstrap();
